54b0233a45df6b5211d746b9c6313b168d683a34
Lavelliane <jhurylastre432@gmail.com>
2025-06-20 15:18:16 +0900

    fix: lint errors

--- src/app/api/v1/registration/[id]/route.ts
+++ src/app/api/v1/registration/[id]/route.ts
@@ -2,23 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { RegistrationApi } from "@/context/registration/infrastructure/registration.api";
 import { deleteRegistration, findRegistrationById, updateRegistration } from "@/context/registration/application/create-registration.application";
 
-export async function GET({ params }: { params: { id: string } }) {
-    const { id } = params;
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     const registrationRepository = RegistrationApi();
     const response = await findRegistrationById(registrationRepository, id);
     return NextResponse.json(response, { status: 200 });
 }
 
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function PUT(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     const body = await request.json();
     const registrationRepository = RegistrationApi();
     const response = await updateRegistration(registrationRepository, id, body);
     return NextResponse.json(response, { status: 200 });
 }
 
-export async function DELETE({ params }: { params: { id: string } }) {
-    const { id } = params;
+export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     const registrationRepository = RegistrationApi();
     const response = await deleteRegistration(registrationRepository, id);
     return NextResponse.json(response, { status: 200 });
